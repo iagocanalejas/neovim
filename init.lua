3e5a38fbff0b176d4eacf169ab274c6df0574e37
Iagocanalejas <iagocanalejas@gmail.com>
2023-06-21 13:43:32 +0200

    use LazyVim config as base

--- init.lua
+++ init.lua
@@ -1,29 +1,24 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
-
--- Make sure to set `mapleader` before lazy so your mappings are correct
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
+require("lazyvim.config").init()
 require("lazy").setup({
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme 'catppuccin-macchiato'
-		end,
+	spec = {
+		{ import = "lazyvim.plugins" },
 	},
-	{ import = "plugins" }
+	defaults = {
+		lazy = false,
+		version = false,
+	},
+	checker = { enabled = true },
 })
-
+require("lazyvim.config").setup({})
