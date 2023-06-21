9c8012546bca504afd824c366e9d79218b6f2dd5
Iagocanalejas <iagocanalejas@gmail.com>
2023-06-21 16:27:05 +0200

    Add fugitive configuration

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -250,6 +250,59 @@ return {
 		end,
 	},
 
+	-- harpoon
+	{
+		"theprimeagen/harpoon",
+		keys = {
+			{
+				"<leader>a",
+				function()
+					return require("harpoon.mark").add_file()
+				end,
+				desc = "[A]dd Harpoon Marker",
+			},
+			{
+				"<C-e>",
+				function()
+					return require("harpoon.ui").toggle_quick_menu()
+				end,
+				desc = "Toggle M[e]nu",
+			},
+			{
+				"<C-a>",
+				function()
+					return require("harpoon.ui").nav_file(1)
+				end,
+				desc = "Harpoon Navigate [1]",
+			},
+			{
+				"<C-s>",
+				function()
+					return require("harpoon.ui").nav_file(2)
+				end,
+				desc = "Harpoon Navigate [2]",
+			},
+			{
+				"<C-d>",
+				function()
+					return require("harpoon.ui").nav_file(3)
+				end,
+				desc = "Harpoon Navigate [3]",
+			},
+			{
+				"<C-f>",
+				function()
+					return require("harpoon.ui").nav_file(4)
+				end,
+				desc = "Harpoon Navigate [4]",
+			},
+		},
+		opts = {
+			exclude_filetyes = { "gitcommit" },
+		},
+		config = true,
+	},
+
 	-- which-key
 	{
 		"folke/which-key.nvim",
@@ -280,46 +333,6 @@ return {
 			wk.setup(opts)
 			wk.register(opts.defaults)
 		end,
-	},
-
-	-- git signs
-	{
-		"lewis6991/gitsigns.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		opts = {
-			signs = {
-				add = { text = "▎" },
-				change = { text = "▎" },
-				delete = { text = "" },
-				topdelete = { text = "" },
-				changedelete = { text = "▎" },
-				untracked = { text = "▎" },
-			},
-			on_attach = function(buffer)
-				local gs = package.loaded.gitsigns
-
-				local function map(mode, l, r, desc)
-					vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
-				end
-
-				map("n", "]h", gs.next_hunk, "Next Hunk")
-				map("n", "[h", gs.prev_hunk, "Prev Hunk")
-				map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-				map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-				map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
-				map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-				map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
-				map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
-				map("n", "<leader>ghb", function()
-					gs.blame_line({ full = true })
-				end, "Blame Line")
-				map("n", "<leader>ghd", gs.diffthis, "Diff This")
-				map("n", "<leader>ghD", function()
-					gs.diffthis("~")
-				end, "Diff This ~")
-				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
-			end,
-		},
 	},
 
 	-- references
