bd03ec59ca5bc7f8761a91c91b8e49571a6f3e50
Iagocanalejas <iagocanalejas@gmail.com>
2023-09-07 11:48:02 +0200

    plugin changes

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,85 +1,93 @@
 local disallowed_paths = { "*/build/*" }
 
 local function get_referenced_item_path(completion_item)
-	local path = nil
-	if completion_item.labelDetails then
-		path = ""
-		if completion_item.labelDetails.description then
-			path = path .. completion_item.labelDetails.description
-		end
-	end
-	return path
+    local path = nil
+    if completion_item.labelDetails then
+        path = ""
+        if completion_item.labelDetails.description then
+            path = path .. completion_item.labelDetails.description
+        end
+    end
+    return path
 end
 
 local match_path = function(patterns, path)
-	-- modules, at least in python, separated by dot, unlike paths
-	path = path:gsub("%.", "/")
-	-- normalize to always start with slash
-	if path:sub(1, 1) ~= "/" then
-		path = "/" .. path
-	end
-	for _, pattern in ipairs(patterns) do
-		-- Convert glob-like pattern to Lua pattern
-		pattern = pattern:gsub("%*", ".*"):gsub("%-", "%%-"):gsub("%/", "/")
-		-- Add anchors to start and end
-		pattern = "^" .. pattern .. "$"
+    -- modules, at least in python, separated by dot, unlike paths
+    path = path:gsub("%.", "/")
+    -- normalize to always start with slash
+    if path:sub(1, 1) ~= "/" then
+        path = "/" .. path
+    end
+    for _, pattern in ipairs(patterns) do
+        -- Convert glob-like pattern to Lua pattern
+        pattern = pattern:gsub("%*", ".*"):gsub("%-", "%%-"):gsub("%/", "/")
+        -- Add anchors to start and end
+        pattern = "^" .. pattern .. "$"
 
-		if string.match(path, pattern) then
-			return true
-		else
-		end
-	end
-	return false
+        if string.match(path, pattern) then
+            return true
+        else
+        end
+    end
+    return false
 end
 
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-nvim-lua" },
-		},
-		config = function()
-			local cmp = require("cmp")
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lua" },
+            {
+                "L3MON4D3/LuaSnip",
+                event = "InsertEnter",
+                dependencies = { "rafamadriz/friendly-snippets" },
+            },
+        },
+        config = function()
+            require("luasnip/loaders/from_vscode").lazy_load()
+            local cmp = require("cmp")
 
-			cmp.setup({
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "luasnip" },
-					{
-						name = "nvim_lsp",
-						entry_filter = function(entry, ctx)
-							local path = get_referenced_item_path(entry.completion_item)
-							if path ~= nil then
-								if match_path(disallowed_paths, path) then
-									return false
-								end
-							end
-							return true
-						end,
-					},
-					{ name = "nvim_lua" },
-					{ name = "buffer", keyword_length = 4 },
-					{ name = "path" },
-				}),
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				experimental = {
-					ghost_text = true,
-				},
-			})
-		end,
-	},
+            cmp.setup({
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm(),
+                    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "luasnip" },
+                    {
+                        name = "nvim_lsp",
+                        entry_filter = function(entry, ctx)
+                            local path = get_referenced_item_path(entry.completion_item)
+                            if path ~= nil then
+                                if match_path(disallowed_paths, path) then
+                                    return false
+                                end
+                            end
+                            return true
+                        end,
+                    },
+                    { name = "vim-dadbod-completion" },
+                    { name = "nvim_lua" },
+                    { name = "buffer",               keyword_length = 4 },
+                    { name = "path" },
+                }),
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                experimental = {
+                    ghost_text = true,
+                },
+            })
+        end,
+    },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,236 +1,46 @@
--- LSP Server Settings
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
----@type lspconfig.options
-local servers = {
-	jsonls = {
-		-- lazy-load schemastore when needed
-		on_new_config = function(new_config)
-			new_config.settings.json.schemas = new_config.settings.json.schemas or {}
-			vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
-		end,
-		settings = {
-			json = {
-				format = {
-					enable = true,
-				},
-				validate = { enable = true },
-			},
-		},
-		capabilities = capabilities,
-	},
-	lua_ls = {
-		settings = {
-			Lua = {
-				workspace = {
-					checkThirdParty = false,
-				},
-				completion = {
-					callSnippet = "Replace",
-				},
-			},
-		},
-		capabilities = capabilities,
-	},
-	marksman = {},
-	tsserver = {
-		settings = {
-			typescript = {
-				format = {
-					indentSize = vim.o.shiftwidth,
-					convertTabsToSpaces = vim.o.expandtab,
-					tabSize = vim.o.tabstop,
-				},
-			},
-			javascript = {
-				format = {
-					indentSize = vim.o.shiftwidth,
-					convertTabsToSpaces = vim.o.expandtab,
-					tabSize = vim.o.tabstop,
-				},
-			},
-			completions = {
-				completeFunctionCalls = true,
-			},
-		},
-		capabilities = capabilities,
-	},
-	tailwindcss = {
-		filetypes_exclude = { "markdown" },
-		capabilities = capabilities,
-	},
-	prismals = {
-		prismaFmtBinPath = "",
-		capabilities = capabilities,
-	},
-	pyright = {},
-	ruff_lsp = {
-		on_attach = function(client, bufnr)
-			-- Enable completion triggered by <c-x><c-o>
-			vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-		end,
-		settings = {
-			args = { "--line-length=120" },
-		},
-		capabilities = capabilities,
-	},
-	yamlls = { capabilities = capabilities },
-}
-
 return {
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v2.x",
-		dependencies = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" },
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+        },
+        config = function()
+            require("lspconfig")
 
-			-- Snippets
-			{ "L3MON4D3/LuaSnip" },
-			{ "rafamadriz/friendly-snippets" },
-		},
-		config = function()
-			local lsp = require("lsp-zero")
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+                callback = function(ev)
+                    -- Enable completion triggered by <c-x><c-o>
+                    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-			lsp.preset("recommended")
+                    -- Buffer local mappings.
+                    -- See `:help vim.lsp.*` for documentation on any of the below functions
+                    local opts = { buffer = ev.buf }
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
+                    vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set('n', '<space>f', function()
+                    vim.lsp.buf.format { async = true }
+                    end, opts)
 
-			lsp.ensure_installed(vim.tbl_keys(servers))
+                    -- Set default LSP formatting
+                    local lsp_events = vim.api.nvim_create_augroup("EpicLspEvents", { clear = true, })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        callback = function() pcall(vim.lsp.buf.format) end,
+                        group = lsp_events,
+                        buffer = 0,
+                        desc = "Format before saving if LSP is attached",
+                    })
+                end,
+            })
 
-			-- Fix Undefined global 'vim'
-			lsp.nvim_workspace()
-
-			local cmp = require("cmp")
-			local cmp_mappings = lsp.defaults.cmp_mappings({
-				["<Cr>"] = cmp.mapping.confirm({ select = true }),
-				["<C-Space>"] = cmp.mapping.complete(),
-			})
-
-			cmp_mappings["<Tab>"] = nil
-			cmp_mappings["<S-Tab>"] = nil
-
-			lsp.setup_nvim_cmp({ mapping = cmp_mappings })
-
-			lsp.set_preferences({
-				suggest_lsp_servers = false,
-				sign_icons = {
-					error = "E",
-					warn = "W",
-					hint = "H",
-					info = "I",
-				},
-			})
-
-			lsp.on_attach(function(client, bufnr)
-				local opts = { buffer = bufnr, noremap = true, silent = true }
-
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-				vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-				vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-				vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-				vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
-				vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
-			end)
-
-			lsp.setup()
-
-			-- setup all the servers
-			local lspconfig = require("lspconfig")
-			lspconfig.jsonls.setup(servers.jsonls)
-			lspconfig.lua_ls.setup(servers.lua_ls)
-			lspconfig.marksman.setup(servers.marksman)
-			lspconfig.tsserver.setup(servers.tsserver)
-			lspconfig.tailwindcss.setup(servers.tailwindcss)
-			lspconfig.prismals.setup(servers.prismals)
-			lspconfig.pyright.setup(servers.pyright)
-			lspconfig.ruff_lsp.setup(servers.ruff_lsp)
-			lspconfig.yamlls.setup(servers.yamlls)
-
-			vim.diagnostic.config({ virtual_text = true })
-		end,
-	},
-
-	-- Mason
-	{
-		"williamboman/mason.nvim",
-		build = ":MasonUpdate", -- :MasonUpdate updates registry contents
-		opts = {
-			ensure_installed = {
-				"black",
-				"prettierd",
-				"pyright",
-				"shfmt",
-				"stylua",
-			},
-		},
-		---@param opts MasonSettings | {ensure_installed: string[]}
-		config = function(_, opts)
-			require("mason").setup(opts)
-			local mr = require("mason-registry")
-
-			-- Install 'ensure_installed' packages
-			local function ensure_installed()
-				for _, tool in ipairs(opts.ensure_installed) do
-					local p = mr.get_package(tool)
-					if not p:is_installed() then
-						p:install()
-					end
-				end
-			end
-			if mr.refresh then
-				mr.refresh(ensure_installed)
-			else
-				ensure_installed()
-			end
-		end,
-	},
-
-	-- Null_ls
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			{ "jose-elias-alvarez/typescript.nvim" },
-		},
-		opts = function()
-			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-			local nls = require("null-ls")
-			return {
-				root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-				sources = {
-					nls.builtins.diagnostics.fish,
-
-					nls.builtins.formatting.fish_indent,
-					nls.builtins.formatting.stylua,
-					nls.builtins.formatting.shfmt,
-					nls.builtins.formatting.prettierd.with({
-						extra_args = { "--max_line_length=120" },
-					}),
-					nls.builtins.formatting.black.with({
-						extra_args = { "--line-length=120" },
-					}),
-					require("typescript.extensions.null-ls.code-actions"),
-				},
-				on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({
-							group = augroup,
-							buffer = bufnr,
-						})
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = augroup,
-							buffer = bufnr,
-							callback = function()
-								vim.lsp.buf.format({ bufnr = bufnr })
-							end,
-						})
-					end
-				end,
-			}
-		end,
-	},
+            vim.diagnostic.config({ virtual_text = true })
+        end,
+    },
 }
